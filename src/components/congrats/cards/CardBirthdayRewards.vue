d5d8d1ad3564172b5b5bc57cc3243edf3ab43912
Logesh Kumar <logesh.kumar@krds.fr>
2020-05-15 08:30:33 +0530

    congrats - setup - transitions

--- src/components/congrats/cards/CardBirthdayRewards.vue
+++ src/components/congrats/cards/CardBirthdayRewards.vue
@@ -1,47 +1,45 @@
 <template>
-  <transition name="slide-fade">
-    <div class="setupItemList">
-      <div class="stepHead">
-        <h3>Birthday Rewards</h3>
-        <p>
-          This event will appear on your site's loyalty program, prompting visitors and members to
-          enter a date
-        </p>
-        <label class="switch" for="card-bdr">
-          <input type="checkbox" v-model="data.status" @change="saveData('events')" id="card-bdr" />
-          <i></i>
-        </label>
-      </div>
-      <div class="setupListBody">
-        <ul class="mt-4 mb-4">
-          <li>{{data.ob_points}} Reward point on birthday</li>
-          <li>{{data.nb_points}} Reward point for entering birthday</li>
-        </ul>
-        <button class="btn btn-outline-dark pr-4 pl-4 mt-4" v-b-modal.modal-bdr>Edit</button>
-      </div>
-      <b-modal
-        id="modal-bdr"
-        hide-header
-        centered
-        modal-class="modal-edit"
-        body-class="d-flex flex-column align-items-center"
-      >
-        <template v-slot:default>
-          <div class="setupContainer">
-            <BirthdayRewards :data="data" :default="defaultSetup.birthday_event" />
-          </div>
-        </template>
-        <template v-slot:modal-footer>
-          <b-button
-            variant="outline-secondary"
-            class="float-right"
-            @click.prevent="cancelModal"
-          >Cancel</b-button>
-          <b-button variant="warning" class="float-right" @click.prevent="updateData">Save & update</b-button>
-        </template>
-      </b-modal>
+  <div class="setupItemList">
+    <div class="stepHead">
+      <h3>Birthday Rewards</h3>
+      <p>
+        This event will appear on your site's loyalty program, prompting visitors and members to
+        enter a date
+      </p>
+      <label class="switch" for="card-bdr">
+        <input type="checkbox" v-model="data.status" @change="saveData('events')" id="card-bdr" />
+        <i></i>
+      </label>
     </div>
-  </transition>
+    <div class="setupListBody">
+      <ul class="mt-4 mb-4">
+        <li>{{data.ob_points}} Reward point on birthday</li>
+        <li>{{data.nb_points}} Reward point for entering birthday</li>
+      </ul>
+      <button class="btn btn-outline-dark pr-4 pl-4 mt-4" v-b-modal.modal-bdr>Edit</button>
+    </div>
+    <b-modal
+      id="modal-bdr"
+      hide-header
+      centered
+      modal-class="modal-edit"
+      body-class="d-flex flex-column align-items-center"
+    >
+      <template v-slot:default>
+        <div class="setupContainer">
+          <BirthdayRewards :data="data" :default="defaultSetup.birthday_event" />
+        </div>
+      </template>
+      <template v-slot:modal-footer>
+        <b-button
+          variant="outline-secondary"
+          class="float-right"
+          @click.prevent="cancelModal"
+        >Cancel</b-button>
+        <b-button variant="warning" class="float-right" @click.prevent="updateData">Save & update</b-button>
+      </template>
+    </b-modal>
+  </div>
 </template>
 <script>
 import BirthdayRewards from "@/components/setup/BirthdayRewards.vue";
--- src/components/congrats/cards/CardTwitterShare.vue
+++ src/components/congrats/cards/CardTwitterShare.vue
@@ -1,50 +1,48 @@
 <template>
-  <transition name="slide-fade">
-    <div class="setupItemList">
-      <div class="stepHead">
-        <h3>Twitter Share Setup</h3>
-        <p>When members share your message on Twitter</p>
-        <label class="switch" for="card-twshare">
-          <input
-            type="checkbox"
-            true-value="1"
-            false-value="0"
-            v-model="data.twitter_tweet.status"
-            @change="saveData('entries')"
-            id="card-twshare"
-          />
-          <i></i>
-        </label>
-      </div>
-      <div class="setupListBody">
-        <ul class="mt-4 mb-4">
-          <li>{{data.twitter_tweet.worth_entries}} Reward Point</li>
-        </ul>
-        <button class="btn btn-outline-dark pr-4 pl-4 mt-4" v-b-modal.modal-tws>Edit</button>
-      </div>
-      <b-modal
-        id="modal-tws"
-        hide-header
-        centered
-        modal-class="modal-edit"
-        body-class="d-flex flex-column align-items-center"
-      >
-        <template v-slot:default>
-          <div class="setupContainer">
-            <TwitterShare :data="data" :default="defaultSetup.twitter_share" />
-          </div>
-        </template>
-        <template v-slot:modal-footer>
-          <b-button
-            variant="outline-secondary"
-            class="float-right"
-            @click.prevent="cancelModal"
-          >Cancel</b-button>
-          <b-button variant="warning" class="float-right" @click.prevent="updateData">Save & update</b-button>
-        </template>
-      </b-modal>
+  <div class="setupItemList">
+    <div class="stepHead">
+      <h3>Twitter Share Setup</h3>
+      <p>When members share your message on Twitter</p>
+      <label class="switch" for="card-twshare">
+        <input
+          type="checkbox"
+          true-value="1"
+          false-value="0"
+          v-model="data.twitter_tweet.status"
+          @change="saveData('entries')"
+          id="card-twshare"
+        />
+        <i></i>
+      </label>
     </div>
-  </transition>
+    <div class="setupListBody">
+      <ul class="mt-4 mb-4">
+        <li>{{data.twitter_tweet.worth_entries}} Reward Point</li>
+      </ul>
+      <button class="btn btn-outline-dark pr-4 pl-4 mt-4" v-b-modal.modal-tws>Edit</button>
+    </div>
+    <b-modal
+      id="modal-tws"
+      hide-header
+      centered
+      modal-class="modal-edit"
+      body-class="d-flex flex-column align-items-center"
+    >
+      <template v-slot:default>
+        <div class="setupContainer">
+          <TwitterShare :data="data" :default="defaultSetup.twitter_share" />
+        </div>
+      </template>
+      <template v-slot:modal-footer>
+        <b-button
+          variant="outline-secondary"
+          class="float-right"
+          @click.prevent="cancelModal"
+        >Cancel</b-button>
+        <b-button variant="warning" class="float-right" @click.prevent="updateData">Save & update</b-button>
+      </template>
+    </b-modal>
+  </div>
 </template>
 <script>
 import TwitterShare from "@/components/setup/TwitterShare";
